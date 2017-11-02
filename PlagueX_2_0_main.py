3b7638cc8305ac3e9f7394f70876024d2d091d5a
Kaushik <koushik.varma_ug18@ashoka.edu.in>
2017-11-02 21:32:43 +0530

    Update and rename main 2.0.py to PlagueX_2_0_main.py

--- PlagueX_2_0_main.py
+++ PlagueX_2_0_main.py
@@ -1,9 +1,9 @@
 import os
 import glob
-import tokenizer
-import concept_corpus
-import mini_corpus
-import sem_eval
+import PlagueX_2_0_tokenizer
+import PlagueX_2_0_concept_corpus
+import PlagueX_2_0_mini_corpus
+import PlagueX_2_0_sem_eval
 import itertools
 raw_text = []
 filenames = []
@@ -13,13 +13,13 @@ for infile in glob.glob( os.path.join('Test Cases/', '*.txt') ):
     raw_text.append(f.read())
 
 file_combs =            list(itertools.combinations(range(len(raw_text)), 2))
-tokenized_objects =     list(map(lambda text: tokenizer.Tokenizer(text),raw_text))
+tokenized_objects =     list(map(lambda text: PlagueX_2_0_tokenizer.Tokenizer(text),raw_text))
 token_data =            list(map(lambda token_object: token_object.tokenized_text, tokenized_objects ))
 nounset_list =          list(map(lambda token_object: token_object.nounset, tokenized_objects ))
 noun_indices =          list(map(lambda token_object: token_object.noun_index, tokenized_objects ))
-primary_corpus = concept_corpus.Concept_Corpus(nounset_list)
-secodary_corpus = mini_corpus.Mini_Corpus(primary_corpus.doc_data,noun_indices)
-semantic_evaluation = sem_eval.Sem_Eval(secodary_corpus.query_data,token_data)
+primary_corpus = PlagueX_2_0_concept_corpus.Concept_Corpus(nounset_list)
+secodary_corpus = PlagueX_2_0_mini_corpus.Mini_Corpus(primary_corpus.doc_data,noun_indices)
+semantic_evaluation = PlagueX_2_0_sem_eval.Sem_Eval(secodary_corpus.query_data,token_data)
 final_scores = semantic_evaluation.score_data
 
 
